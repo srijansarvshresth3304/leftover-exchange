34f16cf3f7d4bfebf1f1d0a587bc516d9f2eccc7
Srijan Sarvshresth <srijansarvshresth@outlook.com>
2026-03-24 04:42:29 +0530

    added search and category filters 🔍🏷️

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vmpwqqivzltfexkdyivl.supabase.co', // Maine aapka ID yahan daal diya hai
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+  /* Aap yahan aur options bhi add kar sakte hain */
 };
 
 export default nextConfig;
